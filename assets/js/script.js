c2f58ff5b13fe00031a7c1cf69c6426170602b4c
nikoletta <grishaberN@outlook.com>
2023-08-30 14:57:16 +0200

    Fix API_KEY value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "iFSipUVzlpvNoPfXiMdU6rYOgql";
+const API_KEY = "iFSipUVzlpvNoPfXiMdU6rYOgqI";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
@@ -13,7 +13,7 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        console.log(data);
     }
 
 }
